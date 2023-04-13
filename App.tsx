576960e21deed3a9bc9a88b5ca730f2fee031e5b
Juan Sánchez <jlsc92@gmail.com>
2023-04-13 17:05:43 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
